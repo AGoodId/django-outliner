1e07902a3fb319fc400a28f29f7320b083e6c7e3
Henrik Almer <henrik@agoodid.se>
2012-02-01 20:56:19 +0100

    Updated comments and regenerated docs

--- outliner/__init__.py
+++ outliner/__init__.py
@@ -4,4 +4,7 @@
               interface of nested structures in the Django admin.
 
 .. moduleauthor:: Henrik Almer <henrik@agoodid.se>
+
+A module that allows an outliner style drag and drop interface of nested
+structures in the Django admin.
 """
